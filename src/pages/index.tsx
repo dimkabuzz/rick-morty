88cb27afc94ef97d58fb2b397734d8266fb1f8a5
dimkabuzz <dimkabuzz@gmail.com>
2022-09-17 19:50:04 +0200

    render first page of characters

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,45 @@
 import { Fragment } from 'react';
 import type { NextPage } from 'next';
+import { GetStaticProps, InferGetStaticPropsType } from 'next';
 import Image from 'next/image';
 
 import Metadata from '@/layouts/Metadata';
 import Container from '@/layouts/Container';
 
-const Home: NextPage = () => {
+type CharacterInfo = {
+  count: number;
+  pages: number;
+  next: string | null;
+  prev: string | null;
+};
+
+type CharacterResults = {
+  id: number;
+  name: string;
+  status: string;
+  species: string;
+  type: string;
+  gender: string;
+  origin: {
+    name: string;
+    url: string;
+  };
+  location: {
+    name: string;
+    url: string;
+  };
+  image: string;
+  episode: string[];
+  url: string;
+  created: string;
+};
+
+type CharacterData = {
+  info: CharacterInfo;
+  results: CharacterResults[];
+};
+
+const Home: NextPage<CharacterData> = ({ info, results }) => {
   return (
     <Fragment>
       <Metadata
@@ -23,69 +57,40 @@ const Home: NextPage = () => {
           <button className="search__btn">Search</button>
         </form>
         <div className="character-list">
-          <article className="character">
-            <div className="character__img">
-              <Image
-                src="/images/1.jpeg"
-                layout="responsive"
-                width={245}
-                height={245}
-                alt=""
-              />
-            </div>
-            <div className="character__text">
-              <h3>Rick Sanchez</h3>
-            </div>
-          </article>
-
-          <article className="character">
-            <div className="character__img">
-              <Image
-                src="/images/1.jpeg"
-                layout="responsive"
-                width={245}
-                height={245}
-                alt=""
-              />
-            </div>
-            <div className="character__text">
-              <h3>Rick Sanchez</h3>
-            </div>
-          </article>
-
-          <article className="character">
-            <div className="character__img">
-              <Image
-                src="/images/1.jpeg"
-                layout="responsive"
-                width={245}
-                height={245}
-                alt=""
-              />
-            </div>
-            <div className="character__text">
-              <h3>Rick Sanchez</h3>
-            </div>
-          </article>
-
-          <article className="character">
-            <div className="character__img">
-              <Image
-                src="/images/1.jpeg"
-                layout="responsive"
-                width={245}
-                height={245}
-                alt=""
-              />
-            </div>
-            <div className="character__text">
-              <h3>Rick Sanchez</h3>
-            </div>
-          </article>
+          {results.map(character => (
+            <article className="character" key={character.id}>
+              <div className="character__img">
+                <Image
+                  src={character.image}
+                  priority
+                  layout="responsive"
+                  width={245}
+                  height={245}
+                  alt=""
+                />
+              </div>
+              <div className="character__text">
+                <h3>{character.name}</h3>
+              </div>
+            </article>
+          ))}
         </div>
       </Container>
     </Fragment>
   );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const res = await fetch('https://rickandmortyapi.com/api/character');
+  const data: CharacterData = await res.json();
+
+  return {
+    props: {
+      info: data.info,
+      results: data.results,
+    },
+    revalidate: 60,
+  };
 };
 
 export default Home;

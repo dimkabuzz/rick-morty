c3cd76d2923cc7ecabe33761f685e807538a464b
dimkabuzz <dimkabuzz@gmail.com>
2022-09-18 16:31:16 +0200

    Favorite button

--- src/common/components/elements/CharacterModal.tsx
+++ src/common/components/elements/CharacterModal.tsx
@@ -33,6 +33,16 @@ export default function CharacterModal({ character, closeModal }: Props) {
         <p>
           <span>Species:</span> {character.species}
         </p>
+        <button className="character-modal__fav-btn">
+          <svg
+            className="character-modal__fav-icon"
+            width="21"
+            height="21"
+            viewBox="0 0 24 24"
+          >
+            <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
+          </svg>
+        </button>
       </div>
       <button className="btn btn--more">Show More</button>
       <button className="character-modal__close-btn" onClick={closeModal}>

32cdf8e396fe4cca8c7425a2f65625c15b4c6bc9
dimkabuzz <dimkabuzz@gmail.com>
2022-09-20 14:52:38 +0200

    ID type fix

--- src/common/utils/fetchCharacter.ts
+++ src/common/utils/fetchCharacter.ts
@@ -1,7 +1,7 @@
 import type { CharacterResults } from '@/types/Character';
 import { API } from '@/utils/constants';
 
-const fetchCharacter = async (id: number) => {
+const fetchCharacter = async (id: string) => {
   const api = `${API}${id}`;
   const response = await fetch(api);
   const data: CharacterResults = await response.json();
